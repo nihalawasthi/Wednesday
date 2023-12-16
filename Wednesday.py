f8bc269acaa5da8ed30e4ad6b12f3423f3d7adf7
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2023-12-16 12:31:19 +0530

    Update Wednesday.py

--- Wednesday.py
+++ Wednesday.py
@@ -1,15 +1,20 @@
-import openai
+import google.generativeai as genai
 import pyttsx3
 import os
 import platform
 import getpass
 
-os.system("cat  ./wed.txt")
 
-openai.api_key = "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"
+os.system("type wed.txt")
+
+api_key = "AIzaSyDiASYl6QZZ-uEOssfFt0vYG3AN_PTxfyk"
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-pro')
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
 
 app_paths = {
     "files": "explorer",
@@ -20,54 +25,69 @@ app_paths = {
     "spotify": "Spotify.exe",
     "browser": "Thorium.exe",
     "cmd": "cmd",
-    "": "",
-    "": "",
-    "": "",
+    "vscode": "code",
+    "whatsapp": "whatsapp",
 }
+
+commands = ["date", "time", "wikipedia", "open app",
+            "open mail", "open youtube", "gemini", "exit"]
+
 
 def date_():
     if platform.system() == "Windows":
         os.system("date /T")
     elif platform.system() == "Linux":
         os.system("date +'%A, %B %d, %Y'")
-    
+
+
 def time_():
     if platform.system() == "Windows":
-        os.system("time")
+        os.system("time /T")
     elif platform.system() == "Linux":
         os.system("date +%T")
-            
+
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 
 def listen(user):
-    user_input = input(f"{user}: ")
-    return user_input.lower()
+    user_input = input(f"{user}: ").lower()
+    return user_input
 
 
 def wish_me(user):
-    if user== "Morpheus":
-        speak("Enter your passcode")
-        pass_code = getpass.getpass(" ")
-        if pass_code.lower() == "wednesday":
-            admin_access()
-        elif pass_code.lower() != "wednesday":
-            print("Intruder detected, closing")
-            speak("Intruder detected, closing")
-            raise SystemExit()
+    if not user:
+        user = input("Username cannot be empty. Please enter your username: ")
+    if user=="Morpheus":
+        admin_access()
     else:
         speak(f"Hello {user}")
         print(f"Hello {user}")
-        
+
 
 def admin_access():
-    print(f"Admin access granted, Welcome Master.")
-    speak(f"Admin access granted, Welcome Master.")
+    speak("Enter your passcode")
+    pass_code = getpass.getpass(" ")
+    if pass_code.lower() != "wednesday":
+        print("Incorrect passcode. Access denied.")
+        raise SystemExit()
+    else:
+        print(f"Admin access granted, Welcome Master.")
+        speak(f"Admin access granted, Welcome Master.")
 
 
 def output(statement, user):
+    if not statement:
+        print("Prompt must not be empty")
+        return
+    
+    if "help" in statement or "list cmd" in statement:
+        speak("Available commands are: ")
+        print("Available commands: ", commands)
+        speak(commands)
+        
     if "shutdown" in statement:
         if platform.system() == "Windows":
             os.system("shutdown /s /t 1")
@@ -85,14 +105,13 @@ def output(statement, user):
             os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
         elif platform.system() == "Linux":
             os.system("sudo systemctl suspend")
-    
+            
     elif "wikipedia" in statement:
         query = statement.replace("wikipedia", "")
         print(f"Searching Wikipedia for '{query}'...")
-    
+        
     elif "open app" in statement:
         app_name = statement.replace("open app", "").strip()
-        print(app_name)
         if app_name in app_paths:
             os.system(app_paths[app_name])
         else:
@@ -100,7 +119,7 @@ def output(statement, user):
             
     elif "open kali" in statement:
         os.system("kali")
-    
+        
     elif "open youtube" in statement:
         if platform.system() == "Windows":
             os.system("start https://www.youtube.com")
@@ -112,7 +131,7 @@ def output(statement, user):
             os.system("start https://mail.google.com")
         elif platform.system() == "Linux":
             os.system("xdg-open https://mail.google.com")
-    
+            
     elif "time" in statement:
         str_time = str(time_())
         speak(f"The time is {str_time}")
@@ -120,55 +139,56 @@ def output(statement, user):
     elif "date" in statement:
         str_date = str(date_())
         speak(f"The date is {str_date}")
-
-    elif "gpt" in statement:
-        print("Initiating ChatGPT ...")
-        speak("Initiating ChatGPT")
-        print("Enter 'exit' to quit Chat")
         
+    elif "gemini" in statement:
+        print("Initiating Gemini...")
+        speak("Initiating Gemini")
+        print("Enter 'exit' to quit Chat")
         while True:
-            user_input = input("User: ")
-            if user_input.lower() == 'exit':
+            user_input = input(f"{user}: ").lower()
+            
+            if user_input == "exit":
                 print("Wednesday: Exiting GPT")
                 speak("Wednesday: Exiting GPT")
                 break
-
-            prompt_text = user_input + "\nWednesday:"
-
+            if not user_input:
+                continue
             try:
-                response = openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=prompt_text,
-                    max_tokens=5000
-                )
-                
-                reply = response.choices[0].text.strip()
+                response = model.generate_content(user_input)
+                reply = response.text.strip()
                 print(f"Wednesday: {reply}\n")
-                speak(reply)
-                
+                for line in reply.splitlines():
+                    speak(line)
             except Exception as e:
                 print(f"An error occurred: {e}")
-            
+                
     elif "who are you" in statement or "what can you do" in statement:
         speak(
-            "I am Wednesday, your personal assistant. I am still under development, but I can perform various tasks like opening applications, searching the web, playing music, and taking photos."
+            "I am Wednesday, your personal assistant. I am still under development, but I can perform various tasks like opening applications, searching the web, playing music, and taking notes."
         )
-    
+        
     elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-        speak("I was created by Nihal")
-    
+        speak("I was created by Nihal.")
+        
     elif "exit" in statement:
         print("Wednesday is shutting down, Goodbye")
         speak("Wednesday is shutting down, Goodbye")
         raise SystemExit()
     
+    else:
+        response = model.generate_content(statement)
+        reply = response.text.strip()
+        print(f"Wednesday: {reply}\n")
+        for line in reply.splitlines():
+            speak(line)
+
 print("Unleashing Wednesday...")
 speak("Unleashing Wednesday...")
 user = input("Please tell your identity: ")
 
-if user == "None":
+if not user:
     user = input("Please tell your identity : ")
-
+    
 print(f"User: {user}\n")
 wish_me(user)
 
